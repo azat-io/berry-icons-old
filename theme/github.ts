b84af392a3e992b45972a00ec902794abbded006
Azat S <to@azat.io>
2023-02-23 19:45:16 +0300

    feat: add github theme

---
+++ theme/github.ts
@@ -0,0 +1,10 @@
+export let github = [
+  '#0d1117' /* black */,
+  '#ecf2f8' /* white */,
+  '#fa7970' /* red */,
+  '#faa356' /* orange */,
+  '#7ce38b' /* green */,
+  '#a2d2fb' /* light blue */,
+  '#77bdfb' /* blue */,
+  '#cea5fb' /* purple */,
+]
