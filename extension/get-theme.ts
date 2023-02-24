7ba8c731a093324c5dea853cd937a46862b79b85
Azat S <to@azat.io>
2023-02-24 13:07:43 +0300

    feat: allow to select icon theme

--- extension/get-theme.ts
+++ extension/get-theme.ts
@@ -1,5 +1,20 @@
+import { workspace } from 'vscode'
+
 import { gruvbox } from '../theme/gruvbox.js'
+import { github } from '../theme/github.js'
+
+type Theme = 'default' | 'github' | 'gruvbox'
 
 export let getTheme = (): string[] => {
-  return gruvbox
+  let theme = workspace.getConfiguration('berry-icons').get<Theme>('theme')
+  if (!theme) {
+    return []
+  }
+  let themes = {
+    default: [],
+    github,
+    gruvbox,
+  }
+
+  return themes[theme]
 }
